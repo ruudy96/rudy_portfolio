35d8865f506ccb786e4f64977cb6fd06355bd7a3
rudysetyawan <ruudysetyawan01@gmail.com>
2024-11-28 17:32:35 +0700

    four commit

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
             <div className="container mx-auto mt-2 md:mt-40 p-2 md:pt-10 ">
                 <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input">
                     <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center">
-                       Contac Me
+                       Contact Me
                     </h2>
                     <form className="my-8" onSubmit={handleSubmit}>
                         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
